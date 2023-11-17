de87538ae7ca63153735d332a8a2db7410352798
Daffa Maulana Wisesa <daffamaulanawisesa@gmail.com>
2023-11-18 04:43:19 +0700

    Files Added (API and Login Form)

---
+++ Modul9_TugasAPI/Modul9_TugasAPI/Data/AccountStore.cs
@@ -0,0 +1,13 @@
+﻿using Modul9_TugasAPI.Models.Dto;
+
+namespace Modul9_TugasAPI.Data
+{
+    public static class AccountStore
+    {
+        public static List<AccountDTO> accountList = new List<AccountDTO>
+        {
+             new AccountDTO{Id=1, Username="daffa", Password="rinrin"},
+             new AccountDTO{Id=2, Username="fadel", Password="levylevy"}
+        };
+    }
+}
---
+++ Modul9_TugasAPI/Modul9_TugasAPI/Models/Dto/AccountDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Modul9_TugasAPI.Models.Dto
+{
+    public class AccountDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
